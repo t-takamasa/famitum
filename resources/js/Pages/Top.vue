23ce184276b2cded68d60a9b61619e8bffdcbe96
t.miyagawa <t.miyagawa@bolster.jp>
2024-10-10 14:11:20 +0900

    vueを作成

---
+++ resources/js/Pages/Top.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <h1>トップページ</h1>
+      <p>ここにコンテンツを追加します。</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Top',
+  };
+  </script>
+  
+  <style scoped>
+  /* スタイルを追加 */
+  </style>
